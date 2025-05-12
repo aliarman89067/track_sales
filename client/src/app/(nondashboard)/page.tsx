f3769e60cc8d00c7d3fbd0cc4f1a9596bb3127a9
ali arman <ali@gmail.com>
2025-05-12 13:46:08 -0600

    Complete landing page model

--- client/src/app/(nondashboard)/page.tsx
+++ client/src/app/(nondashboard)/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Header } from "./Header";
 import { DetailsSection } from "./DetailsSection";
+import AgentHistoryPanel from "./AgentHistoryPanel";
 
 const LandingPage = () => {
   return (
     <section className="flex flex-col w-full gap-2">
       <Header />
       <DetailsSection />
+      <AgentHistoryPanel />
     </section>
   );
 };
